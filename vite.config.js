ea07162e4e09d530e6976fd0be59ef457e397bdf
waiwai2525 <waiwai.cps@gmail.com>
2023-08-08 02:28:43 +0900

    ベースパスを設定

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
+import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 
-export default {
+export default defineConfig({
+  base: process.env.GITHUB_PAGES ? "RocketAR" : "./",
   plugins: [basicSsl()],
-};
+});
